0bed42345bb558779148318cd87c53cbc192bb85
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-04-07 13:25:51 +0200

    feat: backend add feature login

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -1,24 +1,52 @@
 const User = require("../models/User");
 const { createSecretToken } = require("../util/secret-token");
+const bcrypt = require("bcrypt");
 
-module.exports.signup = async (req, res, next) => {
-    try {
-        const { email, password, username, createdAt } = req.body;
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.json({ message: "User already exists" });
+module.exports = {
+    signup: async (req, res, next) => {
+        try {
+            const { email, password, username, createdAt } = req.body;
+            const existingUser = await User.findOne({ email });
+            if (existingUser) {
+                return res.json({ message: "User already exists" });
+            }
+            const user = new User({ email, password, username, createdAt });
+            await user.save();
+            const token = createSecretToken(user._id);
+            res.cookie("token", token, {
+                withCredentials: true,
+                httpOnly: false,
+            });
+            res.status(201)
+                .json({ message: "User signed in successfully", success: true, user });
+            next();
+        } catch (error) {
+            console.error(error);
         }
-        const user = new User({ email, password, username, createdAt });
-        await user.save();
-        const token = createSecretToken(user._id);
-        res.cookie("token", token, {
-            withCredentials: true,
-            httpOnly: false,
-        });
-        res.status(201)
-            .json({ message: "User signed in successfully", success: true, user });
-        next();
-    } catch (error) {
-        console.error(error);
+    },
+    login: async (req, res, next) => {
+        try {
+            const { email, password } = req.body;
+            if(!email || !password ){
+                return res.json({message:'All fields are required'})
+            }
+            const user = await User.findOne({ email });
+            if(!user){
+                return res.json({message:'Incorrect password or email' })
+            }
+            const auth = await bcrypt.compare(password,user.password)
+            if (!auth) {
+                return res.json({message:'Incorrect password or email' })
+            }
+            const token = createSecretToken(user._id);
+            res.cookie("token", token, {
+                withCredentials: true,
+                httpOnly: false,
+            });
+            res.status(201).json({ message: "User logged in successfully", success: true });
+            next()
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const {signup} = require("../controllers/auth-controller");
+const {signup, login} = require("../controllers/auth-controller");
 
 const router = express.Router();
 
 // route for User authentication
 router.post("/signup", signup);
+
+router.post("/login", login);
 
 module.exports = router;
