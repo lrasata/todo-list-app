d66ea9d042b6099568d59795cf4732e0cda31aa9
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-04-18 11:44:53 +0200

    fix: backend cookie setup

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -40,8 +40,9 @@ module.exports = {
             }
             const token = createSecretToken(user._id);
             res.cookie("token", token, {
-                withCredentials: true,
-                httpOnly: false,
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'None'
             });
             res.status(201).json({ message: "User logged in successfully", success: true });
             next()
