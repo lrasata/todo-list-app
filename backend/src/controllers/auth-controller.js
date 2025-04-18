20abc1c745300d00f875c39bbdaae9a97eda5d0d
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-04-18 12:20:28 +0200

    fix: auth-controller to correctly set cookie

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -14,9 +14,9 @@ module.exports = {
             await user.save();
             const token = createSecretToken(user._id);
             res.cookie("token", token, {
-                withCredentials: true,
                 httpOnly: false,
                 secure: process.env.NODE_ENV === 'production',
+                path: '/',
             });
             res.status(201)
                 .json({ message: "User signed in successfully", success: true, user });
@@ -41,9 +41,9 @@ module.exports = {
             }
             const token = createSecretToken(user._id);
             res.cookie("token", token, {
-                withCredentials: true,
                 httpOnly: false,
                 secure: process.env.NODE_ENV === 'production',
+                path: '/',
             });
             res.status(201).json({ message: "User logged in successfully", success: true });
             next()
