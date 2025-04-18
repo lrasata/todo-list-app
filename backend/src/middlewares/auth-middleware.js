9382a475bf1a920aa04771261df4f83db0f788d9
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-04-18 11:28:18 +0200

    fix: add comprehensive message to backend

--- backend/src/middlewares/auth-middleware.js
+++ backend/src/middlewares/auth-middleware.js
@@ -7,7 +7,7 @@ module.exports = {
         const token = req.cookies.token;
         if (!token) {
             return res.status(403)
-                .json({message: "Your are not authorized to perform this action"});
+                .json({message: "Missing token. Your are not authorized to perform this action"});
         }
         try {
             jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
